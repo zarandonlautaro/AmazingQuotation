fc343727e9c517feaaaa4e594566d23f0cb71343
zarandonlautaro <zarandonlautaro@mail.com>
2020-11-14 19:06:49 -0300

    :white_check_mark: revision

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace AmazingQuotation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Quotation());
+            Application.Run(new Form());
 
         }
     }
